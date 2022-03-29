12c113fa36594b7dff55dec949bdb084b7ded6c5
Olav Vahtras <vahtras@kth.se>
2022-03-29 16:39:12 +0200

    Loop over ages

--- savings.py
+++ savings.py
@@ -3,14 +3,10 @@ INTEREST = 0.10
 
 amount = 0.0
 
-# after one year
-amount = amount + 12*MONTHLY
-amount = amount * (1 + INTEREST)
 
-print(amount)
-
-# after two years
-amount = amount + 12*MONTHLY
-amount = amount * (1 + INTEREST)
-
-print(amount)
+for age in range(20, 25):
+    amount = amount + 12*MONTHLY
+    amount = amount * (1 + INTEREST)
+    amount = round(amount, 2)
+    
+    print("Savings at age", age, amount)
