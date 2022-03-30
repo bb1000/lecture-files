32fbe9c97866c5102a3b81cd315b132a952668ef
Olav Vahtras <vahtras@kth.se>
2022-03-30 10:37:42 +0200

    introduce command line arguments

--- savings.py
+++ savings.py
@@ -1,11 +1,19 @@
+import sys
+
 MONTHLY = 500
 INTEREST = 0.10 
 RETIREMENT = 70
 
 amount = 0.0
 
-start_age = int(input("Start saving at age: "))
-stop_age = int(input("Stop saving at age: "))
+print(sys.argv)
+
+if len(sys.argv) < 3:
+    start_age = int(input("Start saving at age: "))
+    stop_age = int(input("Stop saving at age: "))
+else:
+    start_age = int(sys.argv[1])
+    stop_age = int(sys.argv[2])
 
 for age in range(start_age, RETIREMENT):
     if age < stop_age:
@@ -13,4 +21,4 @@ for age in range(start_age, RETIREMENT):
     amount = amount * (1 + INTEREST)
     amount = round(amount, 2)
     
-    print("Savings at age", age, amount)
+print("Savings at age", age, amount)
