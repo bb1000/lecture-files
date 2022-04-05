699834a8aefe2d6b78a6ee7c85e22ecf0d0f5674
Olav Vahtras <vahtras@kth.se>
2022-04-05 14:46:55 +0200

    introduce pytest parametrize

--- test_leap.py
+++ test_leap.py
@@ -1,11 +1,18 @@
+import pytest
 from leap import is_leap_year
 
 
-def test_leap_year():
-    assert is_leap_year(2000) == True
-    assert is_leap_year(1999) == False
-    assert is_leap_year(1998) == False
-    assert is_leap_year(1996) == True
-    assert is_leap_year(1900) == False
-    assert is_leap_year(1800) == False
-    assert is_leap_year(1600) == True
+@pytest.mark.parametrize('year,result',
+    [
+        (2000, True),
+        (1999, False),
+        (1998, False),
+        (1996, True),
+        (1900, False),
+        (1800, False),
+        (1600, True),
+    ]
+)
+def test_leap_year(year, result):
+    assert is_leap_year(year) == result
+
