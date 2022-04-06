0f1576a4b4497cba3fdb07b93a7578fed09b9309
Olav Vahtras <vahtras@kth.se>
2022-04-06 11:55:45 +0200

    backup test savings

--- savings.py
+++ savings.py
@@ -4,19 +4,25 @@ MONTHLY = 500
 INTEREST = 0.10 
 RETIREMENT = 70
 
-amount = 0.0
+def main(start_age, stop_age):
+    amount = 0.0
 
-if len(sys.argv) < 3:
-    start_age = int(input("Start saving at age: "))
-    stop_age = int(input("Stop saving at age: "))
-else:
-    start_age = int(sys.argv[1])
-    stop_age = int(sys.argv[2])
+    #if len(sys.argv) < 3:
+    #    start_age = int(input("Start saving at age: "))
+    #    stop_age = int(input("Stop saving at age: "))
+    #else:
+    #    start_age = int(sys.argv[1])
+    #    stop_age = int(sys.argv[2])
 
-for age in range(start_age, RETIREMENT):
-    if age < stop_age:
-        amount = amount + 12*MONTHLY
-    amount = amount * (1 + INTEREST)
-    amount = round(amount, 2)
-    
-print("Savings at age", age, amount)
+    for age in range(start_age, RETIREMENT):
+        if age < stop_age:
+            amount = amount + 12*MONTHLY
+        amount = amount * (1 + INTEREST)
+        amount = round(amount, 2)
+        
+    print("Savings at age", age, amount)
+    return amount
+
+print('__name__ =',__name__)
+if __name__ == '__main__':
+    main(int(sys.argv[1]), int(sys.argv[2]))
---
+++ test_savings.py
@@ -0,0 +1,4 @@
+import savings
+
+def test_main():
+    assert savings.main(20, 25) == 2937024.49
